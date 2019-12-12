58a83df5fdc2f7c67ceb4c58e4b41b442d2bf3b7
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-12 12:43:50 -0500

    some of the answers

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ return sentence.join(' ')
 }
 
 const averageDogAge = (pets) => {
-  // pets.map
+let total = 0
+
+  const dogs = pets.filter((pet)=>(pet.species)==='dog')
+  const ages = dogs.map((dog)=>(dog.age))
+for (age of ages){
+total += age
+}
+
+return (total/ages.length)
 }
 
 const addToDigits = (num1,num2) => {
@@ -11,42 +19,106 @@ const addToDigits = (num1,num2) => {
   let arrNum= strNum.split('')
   const newArr = [];
   for (let i = 0; i< arrNum.length; i++){
+  if (Number(arrNum[i])+num2 >= 10){
+    newArr.push(10 - (Number(arrNum[i])+num2))
+  }
+else{
     newArr.push(Number(arrNum[i])+num2)
-  
+}
   }
   return Number(newArr.join(''))
   }
 
-const nightOwls = () => {
 
+  const nightOwls = (peoples) => {
+
+ return peoples.filter(function(people){
+    if (people.asleep ===false && people.localTime >= 100 && people.localTime <= 400){
+      return people
+    }
+  }
+)
 }
 
-const nap = () => {
-
+const nap = (peoples) => {
+  
+  return peoples.map( peoples.filter(function(people){
+    if (people.asleep ===false && people.localTime >= 100 && people.localTime <= 400){
+      return {
+        name,
+        localTime,
+        asleep : true
+      }
+    }
+  }
+))
 }
 
-const findIndices = () => {
+
+
+
+const findIndices = (item, func) => {
+  const newArr = item.filter(func)
 
 }
 
 const Faqtory = () => {
+  return {
+    questions: [],
+    length : 0,
+  
+  addQuestion : function(question){
+    this.questions[0].text.push(question)
+    this.length ++
+  }
+}}
+
+
+class Dog  {
+  constructor(name, breed, color){
+this.name = name;
+this.breed = breed;
+this.color = color;
+}
+makeNoise(){
+  return 'Woof.'
+}
+}
+
+class GreatDane extends Dog{
+  constructor(name, color){
+super(name, 'Great Dane', color);
+  }
+  makeNoise(){
+    return 'WOOF.'
+  }
+}
+
+class Beagle extends Dog {
+  constructor(name){
+    super(name, 'Beagle', 'white/black/brown');
+      }
+      makeNoise(){
+        return 'Awooooooooo.'
+      }
 
 }
 
+class Pointer extends Dog {
+  constructor(name ,color, points){
+    super(name,'Pointer', color);
+    this.points = points;
+      }
 
-class Dog {
 
-}
-
-class GreatDane {
-
-}
-
-class Beagle {
-
-}
-
-class Pointer {
+      point(){
+        if(this.points===true){
+          return 'Over there!'
+        }
+        else {
+          return 'Woof.'
+        }
+      }
 
 }
 
